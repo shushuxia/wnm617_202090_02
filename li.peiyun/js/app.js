380a86c7743e9433dafaa1528dd532fd7e016f20
shushuxia <60848068+shushuxia@users.noreply.github.com>
2020-10-19 10:57:53 -0400

    styled animal top

--- li.peiyun/js/app.js
+++ li.peiyun/js/app.js
@@ -52,5 +52,20 @@ $(()=>{
       let template = $(target).html();
       $(this).html(template);
    })
+   
+
+
+   $(".tabgroup .tab").on("click",function(e){
+         let index = $(this).index();
+
+         $(this).addClass("active")
+            .siblings().removeClass("active")
+
+         $(this).closest(".tabgroup")
+            .find(".content").eq(index).addClass("active")
+            .siblings().removeClass("active")
+      })
+
 
 })
+
